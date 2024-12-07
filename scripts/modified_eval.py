0e290b87a89d1b076579c29a95d052d87dfefa08
lawrencefeng17 <lawrence.feng2017@gmail.com>
2024-12-07 14:53:52 -0500

    updated modified_eval to support resetting to default texture

--- scripts/modified_eval.py
+++ scripts/modified_eval.py
@@ -47,6 +47,46 @@ def replace_texture_filename_in_mtl(file_name, new_texture):
     with open(file_name, 'w') as f:
         f.writelines(modified_lines)
 
+def reset_texture_filename_in_xml(file_name, old_texture="texture.png"):
+    """
+    Reset the texture filename in a MuJoCo XML file.
+    
+    Args:
+        file_name (str): Path to the .xml file
+        old_texture (str): Old texture filename
+    """
+    tree = ET.parse(file_name)
+    root = tree.getroot()
+    
+    # Find all texture elements
+    for texture in root.findall(".//texture"):
+        texture.set("file", old_texture)
+    
+    tree.write(file_name)
+    
+def reset_texture_filename_in_mtl(file_name, old_texture="texture.png"):
+    """
+    Reset the texture filename in a Wavefront .mtl file.
+    
+    Args:
+        file_name (str): Path to the .mtl file
+        old_texture (str): Old texture filename
+    """
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+    
+    # Replace the texture filename
+    modified_lines = []
+    for line in lines:
+        if line.startswith("map_Kd"):
+            modified_lines.append(f"map_Kd {old_texture}\n")
+        else:
+            modified_lines.append(line)
+    
+    # Write the modified lines back to file
+    with open(file_name, 'w') as f:
+        f.writelines(modified_lines)
+    
 def main():
     """
     Given a color_mapping JSON file, replace the texture .png files in the corresponding .xml and .mtl files.
@@ -91,21 +131,27 @@ def main():
         data = json.load(f)
         for obj, color in data.items():
             object_folder = Path(assets_path) / obj
-            new_texture = f"texture_{str(color)}.png"
-
-            # Update texture in XML file
-            xml_file = object_folder / f"{obj}.xml"
-            replace_texture_filename_in_xml(xml_file, new_texture)
-            
-            # Update texture in MTL file
-            mtl_file = list(Path(object_folder).rglob("*.mtl"))
-            if len(mtl_file) == 1:
-                mtl_file = mtl_file[0]
-            else:
-                print(f"Error: Found {len(mtl_file)} MTL files for {obj}")
+            if color is None: 
+                # Reset texture to default
+                reset_texture_filename_in_xml(object_folder / f"{obj}.xml")
+                reset_texture_filename_in_mtl(object_folder / f"{obj}.mtl")
                 continue
-            
-            replace_texture_filename_in_mtl(mtl_file, new_texture)
+            else:
+                new_texture = f"texture_{str(color)}.png"
+
+                # Update texture in XML file
+                xml_file = object_folder / f"{obj}.xml"
+                replace_texture_filename_in_xml(xml_file, new_texture)
+                
+                # Update texture in MTL file
+                mtl_file = list(Path(object_folder).rglob("*.mtl"))
+                if len(mtl_file) == 1:
+                    mtl_file = mtl_file[0]
+                else:
+                    print(f"Error: Found {len(mtl_file)} MTL files for {obj}")
+                    continue
+                
+                replace_texture_filename_in_mtl(mtl_file, new_texture)
 
     # Run evaluation script
     
